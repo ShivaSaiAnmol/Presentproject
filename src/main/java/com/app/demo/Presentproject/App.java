7f850eddd8cf00ed73b322a547b353ffd1a6cfb1
Anmol <shivasaianmold@gmail.com>
2024-11-21 22:35:57 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/Presentproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Presentproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
